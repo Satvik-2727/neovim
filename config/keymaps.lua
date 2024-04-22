ebf13a536d51cf5cbb99fe52a6cbfa9af2e4ea09
Satvik-2727 <techblogs2021@gmail.com>
2024-04-22 18:20:37 +0530

    Day 2 NeoVim

--- config/keymaps.lua
+++ config/keymaps.lua
@@ -3,3 +3,5 @@ local opts = { noremap = true, silent = true }
 
 keymap.set("n", "<Leader>w", ":update<Return>", opts) -- Save the changes with space w
 keymap.set("n", "<Leader>q", ":quit<Return>", opts) -- Quit by using space q
+keymap.set("n", "<tab>", ":tabnext<Return>", opts)
+keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
